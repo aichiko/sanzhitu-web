fe5027d4287f3c3c6a720f83e93709fb4fd018e2
Apple <apple@Apple.local>
2017-09-20 09:06:14 +0800

    directory

---
+++ src/pages/home/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{message}}</h1>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      message: '23333'
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// import Hello from '@/components/Hello'
+import home from '@/pages/home/home'
 
 Vue.use(Router)
 
@@ -8,8 +9,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      // model: 'home',
+      name: 'home',
+      component: home
     }
   ]
 })
