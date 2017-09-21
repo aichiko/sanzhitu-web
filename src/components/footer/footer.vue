f2aa9e5c619b62609189cb2fd6271b6d9986770f
ash <aichiko66@163.com>
2017-09-21 09:22:43 +0800

    footer

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,42 +2,42 @@
 <div class="alw-footer">
   <div class="alw-wrapper">
     <div class="alw-links">
-        <dl>
-          <dt>{{footerDl[0].title}}</dt>
-          <dd class="link" v-for="(t, i) in footerDl[0].contents">
-            <a href="" target="_blank" data-tab="5">{{t}}</a>
-          </dd>
-        </dl>
-        <dl>
-          <dt>{{footerDl[1].title}}</dt>
-          <dd class="link">
-            <a href="" target="_blank" data-tab="5"  v-for="(t, i) in footerDl[1].contents">{{t}}</a>
-          </dd>
-        </dl>
-        <dl>
-          <dt>联系我们</dt>
-          <dd>
-            <p>地址：广东省中山市东区富湾东路伊电园I栋</p>
-            <p>电话：0760-87989999</p>
-          </dd>
-        </dl>
-        <div class="alw-QRcode">
-          <div class="alw-pic">
-            <p>下载app</p>
-            <img src="http://www.anlewo.com/templates/default/images/new-home/alw_footer_app.png" alt="下载app" />
-          </div>
-          <div class="alw-pic">
-            <p>关于我们</p>
-            <img src="http://www.anlewo.com/templates/default/images/new-home/alw_footer_weixin.png" alt="关于我们" />
-          </div>
+      <dl>
+        <dt>{{footerDl[0].title}}</dt>
+        <dd class="link" v-for="(t, i) in footerDl[0].contents" :key="t">
+          <a href="" target="_blank" data-tab="5">{{t}}</a>
+        </dd>
+      </dl>
+      <dl>
+        <dt>{{footerDl[1].title}}</dt>
+        <dd class="link">
+          <a href="" target="_blank" data-tab="5"  v-for="(t, i) in footerDl[1].contents" :key="t">{{t}}</a>
+        </dd>
+      </dl>
+      <dl>
+        <dt>联系我们</dt>
+        <dd>
+          <p>地址：广东省中山市东区富湾东路伊电园I栋</p>
+          <p>电话：0760-87989999</p>
+        </dd>
+      </dl>
+      <div class="alw-QRcode">
+        <div class="alw-pic">
+          <p>下载app</p>
+          <img src="http://www.anlewo.com/templates/default/images/new-home/alw_footer_app.png" alt="下载app" />
         </div>
+        <div class="alw-pic">
+          <p>关于我们</p>
+          <img src="http://www.anlewo.com/templates/default/images/new-home/alw_footer_weixin.png" alt="关于我们" />
+        </div>
+      </div>
     </div>
     <div class="alw-copyright">
-      <span class="fl">Copyright @2016 广东安乐窝网络科技有限公司 版权所有
+      <span class="float-left">Copyright @2016 广东安乐窝网络科技有限公司 版权所有
         <a href="" target="_blan  k">粤ICP备13031430号</a>
         <a href="" target="_blank">粤工商备P201709000844</a>
       </span>
-      <a class="icon-guohui fr" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44200002005004" target="_blank">粤公网安备 44200002005004号</a>
+      <a class="icon-guohui float-right" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44200002005004" target="_blank">粤公网安备 44200002005004号</a>
     </div>
   </div>
 </div>
@@ -74,7 +74,7 @@ export default {
 
 <style>
 .alw-footer {
-    background: url(./alw_footer_bg.png) no-repeat center top;
+    background: url(../../assets/szt_footer_bg.png) no-repeat center top;
     background-size: cover;
     background-repeat: repeat-x\9;
     height: 290px;
@@ -147,6 +147,6 @@ export default {
 }
 .alw-copyright a.icon-guohui {
     padding-left: 25px;
-    background: url(./alw_icon_guohui.png) no-repeat left 10px;
+    background: url(../../assets/szt_icon_guohui.png) no-repeat left 10px;
 }
 </style>
