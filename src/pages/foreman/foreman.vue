706cfce9ead7f36fa291d7681ba297f6f07ae6aa
qingyunchen <846291219@qq.com>
2017-10-13 09:40:14 +0800

    我是工长登录注册

--- src/pages/foreman/foreman.vue
+++ src/pages/foreman/foreman.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <div>
+  	<span>jdafkjkafjkaf</span>
+  	<router-link :to="{ path: '/foreman/foreLogin' }" tag="a" active-class="active">foreLogin</router-link>
+  	<router-link :to="{ path: '/foreman/foreRegister' }" tag="a" active-class="active">foreRegister</router-link>
+  </div>
 </template>
 
 <script>
