279e32b72061de6a1de5456c9182aa92ac3037f5
qingyunchen <846291219@qq.com>
2017-10-15 17:32:12 +0800

    decoration
    缺最后一部分

--- src/pages/decoration/decoration.vue
+++ src/pages/decoration/decoration.vue
@@ -1,26 +1,34 @@
 <template>
-<div>
+<div style="background:#fff;margin:0 auto;">
   <header class="head-box">
     <img src="../../assets/decoration/banner.png" alt="图片还在加载中">
   </header>
   <dec-title :title="titles[0]"></dec-title>
-  <figure class="fig">
-    <img src="../../assets/decoration/pc_2.jpg" alt="图片还在加载中">
-    <img src="../../assets/decoration/pc_3.jpg" alt="图片还在加载中">
-    <img src="../../assets/decoration/pc_4.jpg" alt="图片还在加载中">
-    <img src="../../assets/decoration/pc_5.jpg" alt="图片还在加载中">
-    <div class="fig-box" @click="openZoosUrl('bchatwin');lr_hidemini();"></div>
-    <div class="fig-box1" @click="openZoosUrl('bchatwin');lr_hidemini();"></div>
-  </figure>
-  <footer class="foot-box">
-    <img src="../../assets/decoration/footer.jpg" alt="">
-    <form action="" id="t_form">
-      <h5>专业设计师创意参考比谁的设计更合意</h5>
-      <input type="text" name="ryname" placeholder="请输入你的姓名">
-      <input type="text" name="ryphone" placeholder="请输入你的手机号码" maxlength="11">
-      <button type="button" onclick="baoming_sub('#t_form')">申请免费量房</button>
-    </form>
-  </footer>
+  <div class="design">
+    <ul class="bx clearfix">
+      <li v-for="(d, index) in design" :class="{odd:index%2==0,mt18:index > 1}">
+        <img :src="d.img" alt="">
+        <p class="opacity"></p>
+        <p><span>{{ d.title }}</span></p>
+      </li>
+    </ul>
+  </div>
+  <dec-title :title="titles[1]"></dec-title>
+  <div class="solve">
+    <ul class="bx clearfix">
+      <li v-for="(s, index) in solve" :class="{mr18: (index+1)%5 != 0,mt18:index > 4}">
+        <img :src="'../../../static/images/decoration/solve'+ num( index ) +'.png'" >
+        <p>{{ s }}</p>
+      </li>
+    </ul>
+  </div>
+  <div class="decApp">
+    <img src="../../../static/images/decoration/decApp.jpg"  alt="图片还在加载中">
+  </div>
+  <dec-title :title="titles[2]"></dec-title>
+  <div class="myChartBox">
+    <div id="myChart" class="bx" style="width: 1000px;height:500px"></div>
+  </div>
 </div>
 
 </template>
@@ -34,176 +42,184 @@ export default {
         {
           title: '整居设计方案',
           sup: '多种搭配，多种选择，多种方案'
+        },
+        {
+          title: '专业解决问题',
+          sup: '我们致力于为您解决以下问题'
+        },
+        {
+          title: '五年质保',
+          sup: '超出您对美好生活的期待'
         }
-      ]
+      ],
+      design: [
+        {
+          title: '地中海风格',
+          img: '../../../static/images/decoration/desgin.png'
+        },
+        {
+          title: '中式风格',
+          img: '../../../static/images/decoration/desgin.png'
+        },
+        {
+          title: '简约风格',
+          img: '../../../static/images/decoration/desgin.png'
+        },
+        {
+          title: '欧式风格',
+          img: '../../../static/images/decoration/desgin.png'
+        }
+      ],
+      solve: ['量房量不准', '设计图不全', '列项列不对', '算量算不准', '报价误差大', '订单全搞错', '线管乱如麻', '物料不对应', '施工总延迟', '装修不透明']
     }
   },
   components: {
     decTitle
+  },
+  methods: {
+    num (index) {
+      return (index + 1) > 9 ? (index + 1) : ('0' + (index + 1))
+    }
+  },
+  mounted () {
+    let myChart = this.$echarts.init(document.getElementById('myChart'))
+    let goverStandard = [3, 3, 1, 1, 1, 1, 1]
+    let sztStandard = [5, 5, 3, 3, 3, 3, 3]
+    myChart.setOption({
+      tooltip: {},
+      legend: {
+        data: ['国家标准', '三只兔标准']
+      },
+      xAxis: {
+        type: 'category',
+        data: ['防水\n工程', '水电\n工程', '墙面\n瓷砖', '木门\n橱柜', '抽油\n烟机', '卫浴\n套装', '结构\n墙面'],
+        axisLabel: {
+          showMinLabel: true,
+          borderWidth: 1,
+          borderColor: '#757374',
+          color: '#595758',
+          padding: [5, 11]
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      yAxis: [{
+        name: '年',
+        min: 0,
+        max: 7,
+        axisTick: {
+          inside: true
+        },
+        splitLine: {
+          show: false
+        }
+      }],
+      series: [{
+        name: '国家标准',
+        type: 'bar',
+        data: goverStandard,
+        barCategoryGap: '40%',
+        itemStyle: {
+          normal: {
+            show: true,
+            color: '#F08300',
+            barBorderRadius: 15,
+            borderWidth: 0,
+            borderColor: '#333'
+          }
+        }
+      }, {
+        name: '三只兔标准',
+        type: 'bar',
+        data: sztStandard,
+        barCategoryGap: '40%',
+        itemStyle: {
+          normal: {
+            show: true,
+            color: '#E70012',
+            barBorderRadius: 15,
+            borderWidth: 0,
+            borderColor: '#333'
+          }
+        }
+      }]
+    })
   }
 }
 </script>
 <style lang="less">
-.head-box {
+.head-box,.decApp {
   width: 100%;
   img {
     width: 100%;
-    height: 100%;
   }
 }
-.nav-box {
-  width: 100%;
-  position: relative;
-  img {
-    width: 100%;
-    height: 100%;
-  }
-  #b_form {
-    width: 20%;
-    height: 50%;
-    position: absolute;
-    top: 34%;
-    right: 18.5%;
-    display: flex;
-    flex-direction: column;
-    align-items: baseline;
-    justify-content: space-between;
-    .box-item {
-      width: 90%;
-      height: 20%;
-      display: flex;
-      justify-content: space-between;
-      input{
-        width: 60%;
-        height: 100%;
-        border: none;
-        border-radius: 8px;
-        font-size: 1.2rem;
-        text-indent: 3em;
-        background: url(../../assets/decoration/name.png) 10% -10% no-repeat white;
+.mt18{
+  margin-top: 18px;
+}
+.mr18{
+  margin-right: 18px;
+}
+.design{
+  .bx{
+    li{
+      width:591px;
+      height:373px;
+      float: left;
+      position: relative;
+      &.odd{
+        margin-right: 18px;
       }
-      select {
-        -webkit-appearance: menulist;
-        box-sizing: border-box;
-        align-items: center;
-        white-space: pre;
-        -webkit-rtl-ordering: logical;
-        color: black;
-        background-color: white;
-        cursor: default;
-        width: 35%;
-        height: 100%;
-        border: none;
-        border-radius: 8px;
-        font-size: 1.2rem;
-        color: #000;
-        padding: 0 0px 0 35px;
-        box-sizing: border-box;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none;
-        option{
-            border: none;
-            border-radius: 8px;
+      img{
+        outline:0;
+      }
+      p{
+        display: inline-block;
+        width:191px;
+        height: 49px;
+        position: absolute;
+        top:50%;
+        right: 0;
+        margin-top: -24.5px;
+        text-align: center;
+        line-height: 49px;
+        &.opacity{
+          background: rgb(254,0,0);
+          filter: alpha(opacity=50);
+          -moz-opacity: 0.5;
+          opacity: 0.5;
+        }
+        span{
+          color: #fff;
+          font-size:24px;
         }
       }
     }
-    input{
-      width: 90%;
-      height: 20%;
-      border: none;
-      border-radius: 8px;
-      text-indent: 3rem;
-      font-size: 1.2rem;
-      background: url(../../assets/decoration/name.png) 10px -70px no-repeat white;
-      &:nth-last-child(2) {
-        background: url("../../assets/decoration/name.png") 10px -152px no-repeat white;
-      }
-    }
-    button {
-      width: 90%;
-      height: 58px;
-      border: none;
-      border-radius: 8px;
-      font: 1.3rem/58px "";
-      text-shadow: 0 1px 20px #f1f1f1;
-      box-shadow: 0 8px 15px #333;
-      background: #f75a2f;
-      color: white;
-      letter-spacing: 0.1rem;
-    }
   }
 }
-.fig {
-  width: 100%;
-  position: relative;
-  img {
-    width: 100%;
-    height: 100%;
-  }
-  .fig-box {
-    position: absolute;
-    bottom: 2.5%;
-    right: 22%;
-    width: 12.5%;
-    height: 2%;
-    border-radius: 10px;
-  }
-  .fig-box1 {
-    position: absolute;
-    top: 15.5%;
-    right: 30.5%;
-    width: 14%;
-    height: 2%;
-    border-radius: 10px;
-  }
-}
-.foot-box {
-  width: 100%;
-  position: relative;
-  img{
-    width: 100%;
-    height: 100%;
-  }
-  #t_form {
-    width: 20%;
-    height: 60%;
-    position: absolute;
-    top: 90px;
-    right: 20%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: baseline;
-    h5{
-      width: 90%;
-      height: 10%;
-      text-align: center;
-      font: 1rem/calc(100% - 90%) "";
-    }
-    input{
-      width: 90%;
-      height: 20%;
-      font: 1.2rem/58px "";
-      text-indent: 3.5rem;
-      border: none;
-      border-radius: 8px;
-      box-shadow: 0 0 5px 1px #ccc;
-      background: url("../../assets/decoration/name.png") no-repeat 10px 10px;
-      &:nth-child(3) {
-        background: url("../../assets/decoration/name.png") no-repeat 10px -155px;
+.solve{
+  padding-bottom:70px;
+  .bx{
+    li{
+      float: left;
+      width: 225px;
+      img{
+        width: 225px;
+        height: 203px;
+        vertical-align:top;
       }
-    }
-    button {
-      width: 90%;
-      height: 20%;
-      color: white;
-      background: linear-gradient(white,#eb1b2b 98%);
-      letter-spacing: 0.2rem;
-      font: bold 1.5rem/58px "";
-      border: none;
-      border-radius: 8px;
-      box-shadow: 0 10px 10px #ccc;
+      p{
+        background-color: #FE0000;
+        width:225px;
+        height: 58px;
+        line-height: 58px;
+        overflow:hidden;
+        word-wrap: nowrap;
+        text-overflow:ellipsis;
+        font-size:25px;
+        color: #fff;
+      }
     }
   }
 }
