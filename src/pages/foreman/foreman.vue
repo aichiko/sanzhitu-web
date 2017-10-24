0e871b7a0dcf1f3eb5cf19b3612039127745b1ec
qingyunchen <846291219@qq.com>
2017-10-24 09:32:59 +0800

    我是工长
    数据请求显示跨域

--- src/pages/foreman/foreman.vue
+++ src/pages/foreman/foreman.vue
@@ -10,36 +10,18 @@
 					<a href="http://www.sanztu.com:80/grzx" style="">个人中心</a>
 				</h2>
 				<ul id="left" class="perleftmenu">
-						<li><a href="http://www.sanztu.com:80/porders/list/my">我的订单</a></li>
+					<li><router-link to="foremanOrder" tag="a">我的订单</router-link></li>
 					<li>
-						<a href="http://www.sanztu.com:80/grzx_personal_info">我的资料</a>
+						<router-link to="foremanDetail" tag="a">我的资料</router-link>
 					</li>
-					<li><a href="http://www.sanztu.com:80/grzx_update_pwd">密码修改</a></li> 
 					<li><a href="http://www.sanztu.com:80/portal/logout">退出</a></li>
 				</ul>
 			</div>
 			<div class="userRt">
-			   <h2 class="asx graborder">我的资料</h2>
-			   <div class="infoms">
-			  	<div class="inox1">
-			      <form id="fm" action="http://www.sanztu.com:80/puser/updateByAccountManage" method="post">
-			      	  <input type="hidden" id="id" name="id" value="17256">
-			      	  <input type="hidden" name="headPic" id="headPic">
-			          <p><lable>姓名: </lable>4</p>
-			          <p><lable>联系电话: </lable>15236965325</p>
-			          <p><lable>地址: </lable></p>
-			       </form>
-			      </div>
-			   </div>
+			  <router-view></router-view>
 			</div>
 		</div>
 	</div>
-	<div  v-if="!$route.meta.isChildren">
-		<span>jdafkjkafjkaf</span>
-		<router-link :to="{ path: 'foreman/foreLogin' }" tag="a" active-class="active">foreLogin</router-link>
-		<router-link :to="{ path: 'foreman/foreRegister' }" tag="a" active-class="active">foreRegister</router-link>
-	</div>
-	<router-view></router-view>
   <footer>
     <web-footer></web-footer>
   </footer>
@@ -62,7 +44,7 @@ export default {
 </script>
 <style lang="less" scoped>
 .index_center {
-  background: #efefef;
+  background: #fff;
   width: 990px;
   margin: 0 auto;
   .urser{
@@ -98,36 +80,6 @@ export default {
 	    float: left;
 	    width: 792px;
 	    min-height: 850px;
-	    .graborder {
-		    border-bottom: 1px solid #dedede;
-		    font-size: 22px;
-		    color: #0081c9;
-		    padding-left: 35px;
-		    line-height: 60px;
-			}
-			.inox1 {
-		    margin: 20px 0px 0px 50px;
-		    form {
-			    margin-bottom: 60px;
-			    margin-top: 30px;
-			    input {
-				    border-radius: 2px;
-				    -moz-border-radius: 2px;
-				    -webkit-border-radius: 2px;
-				    padding: 2px;
-				    color: #666;
-					}
-					p{
-						padding: 8px 0px;
-						text-align:left;
-						lable {
-					    display: inline-block;
-					    width: 110px;
-					    font-size: 14px;
-						}
-					}
-				}
-			}
 		}
 	}
 }
---
+++ src/pages/foreman/foremanChildren/formanDetail.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="box">
+		<h2 class="asx graborder">我的资料</h2>
+		<div class="infoms">
+			<div class="inox1">
+				<form id="fm" action="http://www.sanztu.com:80/puser/updateByAccountManage" method="post">
+				  <input type="hidden" id="id" name="id" value="17256">
+				  <input type="hidden" name="headPic" id="headPic">
+				  <p><span>姓名:</span> 4</p>
+				  <p><span>联系电话:</span>15236965325</p>
+				  <p><span>地址:</span> </p>
+				</form>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="less" scoped>
+.box {
+  width: 792px;
+  min-height: 850px;
+  .graborder {
+    border-bottom: 1px solid #dedede;
+    font-size: 22px;
+    color: #0081c9;
+    padding-left: 35px;
+    line-height: 60px;
+	}
+	.inox1 {
+    margin: 20px 0px 0px 50px;
+    form {
+	    margin-bottom: 60px;
+	    margin-top: 30px;
+	    input {
+		    border-radius: 2px;
+		    -moz-border-radius: 2px;
+		    -webkit-border-radius: 2px;
+		    padding: 2px;
+		    color: #666;
+			}
+			p{
+				padding: 8px 0px;
+				text-align:left;
+				span {
+			    display: inline-block;
+			    width: 110px;
+			    font-size: 14px;
+				}
+			}
+		}
+	}
+}
+</style>
