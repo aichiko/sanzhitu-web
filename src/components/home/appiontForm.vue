a6ee0fdbd00221a38ccc586f239e702310232208
ash <aichiko66@163.com>
2017-11-05 20:28:04 +0800

    首页的预约

--- src/components/home/appiontForm.vue
+++ src/components/home/appiontForm.vue
@@ -1,74 +1,128 @@
 <template>
-      <div class="pe-body">
+    <div>
+        <div class="pe-body">
           <form>
             <p class="clearfix">
                 <select id="province" class="pe-select pe-pro float-left" v-model="provinceValue">
-                    <option :value="province.value" v-for="province in provinces" :key="province.value">{{province.text}}</option>
+                    <option :value="province.id" v-for="province in provinces" :key="province.id">{{province.name}}</option>
                 </select>
                 <select class="pe-select pe-ciy float-right" v-model="cityValue">
-                    <option :value="city.value" v-for="city in citys" :key="city.value">{{city.text}}</option>
+                    <option :value="city.id" v-for="city in citys" :key="city.id">{{city.name}}</option>
                 </select>
             </p>
             <p class="clearfix">
                 <select id="booked_map" class="pe-select pe-map float-left">
-                    <option :value="shop.value" v-for="shop in shops" :key="shop.value">{{shop.text}}</option>
+                    <option :value="shop.id" v-for="shop in shops" :key="shop.id">{{shop.company}}</option>
                 </select>
             </p>
             <p class="pe-input">
-                <input type="tel" id="booked_name" placeholder="您的姓名">
+                <input type="tel" id="booked_name" placeholder="您的姓名" v-model="appointForm.userName">
                 <span class="pe-del"></span>
             </p>
             <p class="pe-input">
-                <input type="tel" id="booked_tel" placeholder="手机号码">
+                <input type="tel" id="booked_tel" placeholder="手机号码" v-model="appointForm.mobile">
                 <span class="pe-del"></span>
             </p>
           </form>
-      </div>
+        </div>
+        <div class="warp-center-btn">
+                <i class="center-btn-logo"></i>
+                <div class="center-btn-submit">
+                <el-button type="primary" @click="appointAction">预约装修</el-button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import {country} from '../../config/country.js'
+import {appointmentRequest} from '../../config/country.js'
 
 export default {
+  props: {
+    countryData: {}
+  },
   data () {
     return {
-      countryData: country,
       provinceIndex: 0,
       cityIndex: 0,
-      provinceValue: '10',
-      cityValue: '11'
+      provinceValue: 1,
+      cityValue: 1,
+      shopValue: 1,
+      shopIndex: 0,
+      citys: {},
+      provinces: {},
+      shops: [],
+      appointForm: {
+        userName: '',
+        mobile: '',
+        province: 1,
+        city: 1,
+        experiencePavilionId: 1,
+        provinceName: '',
+        cityName: '',
+        experiencePavilionName: ''
+      }
     }
   },
   watch: {
+    countryData: function (countryData) {
+      // console.log(this.$store)
+      console.log(countryData)
+      this.provinces = countryData.children
+      this.citys = this.provinces[this.provinceIndex].children
+      this.shops = this.$store.getters.shops
+      this.provinceValue = this.provinces[0].id
+      this.cityValue = this.citys[0].id
+      this.appointForm.experiencePavilionId = this.shops[0].id
+      this.appointForm.experiencePavilionName = this.shops[0].company
+    },
     provinceValue: function (newValue) {
       var values = this.provinces.map(function (item) {
-        return item.value
+        return item.id
       })
       var index = values.indexOf(newValue)
       this.provinceIndex = index
       console.log('provinceIndex === ', index)
+      this.appointForm.provinceName = this.provinces[index].name
+      this.appointForm.province = newValue
       // 需要对cityValue进行赋值 很重要！
-      this.cityValue = this.citys[0].value
+      this.cityValue = this.citys[0].id
     },
     cityValue: function (newValue) {
       var values = this.citys.map(function (item) {
-        return item.value
+        return item.id
       })
       var index = values.indexOf(newValue)
       this.cityIndex = index
+      this.appointForm.cityName = this.citys[index].name
+      this.appointForm.city = newValue
       console.log('cityIndex === ', index)
+    },
+    shopValue: function (newValue) {
+      var values = this.shops.map(function (item) {
+        return item.id
+      })
+      var index = values.indexOf(newValue)
+      this.shopIndex = index
+      this.appointForm.experiencePavilionName = this.shops[index].company
+      this.appointForm.experiencePavilionId = newValue
+      console.log('shopIndex === ', index)
     }
   },
-  computed: {
-    provinces: function () {
-      return this.countryData.provinces
-    },
-    citys: function () {
-      return this.provinces[this.provinceIndex].citys
-    },
-    shops: function () {
-      return this.citys[this.cityIndex].shops
+  methods: {
+    appointAction: function (e) {
+      console.log('免费预约')
+      var that = this
+      appointmentRequest(this.appointForm, function (json) {
+        that.$notify({
+          title: '预约成功',
+          message: '恭喜您，预约成功！',
+          type: 'success'
+        })
+      })
     }
+  },
+  mounted: function () {
   }
 }
 </script>
@@ -195,6 +249,33 @@ export default {
     .pe-submit {
         height: 32px;
         width: 130px;
+    }
+  }
+}
+
+.warp-center-btn {
+    float: left;
+    width: 135px;
+    margin-left: 10px;
+    padding-top: 22px;
+    .center-btn-logo {
+    display: inline-block;
+    margin-top: -10px;
+    width: 135px;
+    height: 111px;
+    background-image: url(../../../static/images/home/bottom-imgs.png);
+    }
+    .center-btn-submit {
+    width: 135px;
+    height: 30px;
+    // background: url(../../../static/images/home/bottom-imgs.png) -17px 0px no-repeat;
+    cursor: pointer;
+
+    margin-top: 2px;
+    .el-button--primary {
+        background-color: #34cc34;
+        color: #fff;
+        border-color: #34cc34
     }
   }
 }
