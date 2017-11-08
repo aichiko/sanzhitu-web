4c8e98ae913ea338a6c88e8c0a51b88058649480
qingyunchen <846291219@qq.com>
2017-11-08 17:05:31 +0800

    订单列表
    翻页成功，但是操作的按钮没成功

--- src/pages/foreman/foremanChildren/formanOrder.vue
+++ src/pages/foreman/foremanChildren/formanOrder.vue
@@ -1,40 +1,61 @@
 <template>
 	<div class="box">
-  <header>
-    <my-header></my-header>
-  </header>
+    <header>
+      <my-header></my-header>
+    </header>
 		<div class="bx">
-    <h2 class="asx graborder">我的订单</h2>
-    <p class="telSel">电话号码：<input type="text" placeholder="输入电话号码查询" /></p>
-    <div class="infoms">
-      <div class="inox1">
-        <el-table
-          :data="tableData"
-          stripe
-          style="width: 100%">
-          <el-table-column
-            prop="date"
-            label="下单日期"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="类型"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="服务项目">
-          </el-table-column>
-        </el-table>
-        <div class="block">
-          <el-pagination
-            layout="prev, pager, next"
-            :total="1000">
-          </el-pagination>
+      <h2 class="title">我的订单</h2>
+      <p class="telSel">联系电话：<input type="text" placeholder="输入电话号码查询" v-model="tel" /><button class="btn" @click="pageTurn">查询</button></p>
+      <div class="infoms">
+        <div class="inox1">
+          <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+            <el-table-column
+              prop="order_time"
+              label="下单日期"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="orderTypeName"
+              label="类型"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="title"
+              label="服务项目">
+            </el-table-column>
+            <el-table-column
+              prop="contact"
+              label="联系人">
+            </el-table-column>
+            <el-table-column
+              prop="contact_phone"
+              label="联系电话">
+            </el-table-column>
+            <el-table-column
+              prop="place"
+              label="服务地址">
+            </el-table-column>
+            <el-table-column
+              prop="statusName"
+              label="订单状态">
+            </el-table-column>
+            <el-table-column
+              prop="substation_name"
+              label="所属分站">
+            </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini">详细</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="cus-center" id="page"><ul class="pagination cus-center" style="cursor: pointer;"><li><a pageno="0" :class="{gray:page <= 0}" @click="back">«</a></li><li><a pageno="1" @click="add" :class="{gray:(this.page + 1) >= (this.total / 10)}">»</a></li></ul></div>
         </div>
       </div>
-    </div>
     </div>
     <footer>
       <web-footer></web-footer>
@@ -51,23 +72,10 @@ import $ from 'jquery'
 export default {
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      tableData: [],
+      tel: '',
+      page: 0,
+      total: 1
     }
   },
   components: {
@@ -75,18 +83,29 @@ export default {
     myHeader: head,
     webFooter: webFooter
   },
-  mounted () {
-    $.ajax({
-      type: 'get',
-      url: 'http://sanztu.com/wz/order/my_noFinish_list?contact_phone=18986336761&size=10&offset=0&jsoncallback=?',
-      dataType: 'jsonp',
-      success: function (data) {
-        console.log(data)
-      },
-      error: function (data) {
-        console.log(data)
+  methods: {
+    pageTurn: function () {
+      var that = this
+      $.getJSON('http://sanztu.com/wz/order/my_noFinish_list?contact_phone=' + that.tel + '&size=10&offset=' + that.page).then((res) => {
+        console.log(res)
+        that.tableData = res.data.lst
+        that.total = res.data.total
+      }, (err) => {
+        console.log(err)
+      })
+    },
+    add () {
+      if ((this.page + 1) < (this.total / 10)) {
+        this.page++
+        this.pageTurn()
       }
-    })
+    },
+    back () {
+      if (this.page > 0) {
+        this.page--
+        this.pageTurn()
+      }
+    }
   }
 }
 </script>
@@ -94,24 +113,69 @@ export default {
 <style lang="less" scoped>
 .box {
   width: 100%;
-  min-height: 100%;
-  .graborder {
-    font-size: 22px;
-    color: #0081c9;
-    padding-left: 35px;
-    line-height: 60px;
-	}
-  .telSel{
-    text-align: left;
-    padding:10px;
-    input{
-      border: 1px solid #dedede;
-      height:30px;
-      text-indent: 10px;
+  .bx{
+    .title {
+      font-size: 22px;
+      color: #0081c9;
+      line-height: 60px;
+      height:60px;
+      white-space:nowrap;
+      text-align: center;
+      width: 100%;
+    }
+    .telSel{
+      text-align: left;
+      width: 100%;
+      padding:10px;
+      input{
+        border: 1px solid #dedede;
+        height:30px;
+        text-indent: 10px;
+      }
+      .btn{
+        color: #fff;
+        background-color: #5cb85c;
+        font-size: 14px;
+        padding:6px 38px;
+        border-radius: 4px;
+        vertical-align:top;
+        margin-left: 10px;
+      }
+    }
+    .infoms {
+      .cus-center {
+        text-align: center;
+        .cus-center {
+          text-align: center;
+          display: inline-block;
+          padding-left: 0;
+          margin: 20px 0;
+          border-radius: 4px;
+          li{
+            display: inline;
+            a{
+              position: relative;
+              float: left;
+              padding: 6px 12px;
+              margin-left: -1px;
+              line-height: 1.42857143;
+              color: #337ab7;
+              text-decoration: none;
+              background-color: #fff;
+              border: 1px solid #ddd;
+              &.gray{
+                background-color: #e9e9e9;
+              }
+            }
+            &:first-child>a{
+              margin-left: 0;
+              border-top-left-radius: 4px;
+              border-bottom-left-radius: 4px;
+            }
+          }
+        }
+      }
     }
   }
-	.infoms {
-    height: 650px;
-	}
 }
 </style>
